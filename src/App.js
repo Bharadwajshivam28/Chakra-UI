f31d88095555f0e5594e0477928f2efdc6b5bea3
Shivam KUmar <shivambharadwaj822@gmail.com>
2023-05-18 01:08:42 +0530

    Created Home Page for Chakra-UI

--- src/App.js
+++ src/App.js
@@ -4,12 +4,12 @@ import Home from "./components/Home";
 
 function App (){
   return (
-  <Router>
-    <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      </Routes>
-  </Router>
+   <Router>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        </Routes>
+    </Router>
   );
 }
 
